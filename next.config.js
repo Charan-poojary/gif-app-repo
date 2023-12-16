38a562810764faec842dd792e7ab8ca79968ba5e
Charan <charanhejamady101@gmail.com>
2023-12-16 18:02:31 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-// next.config.js
 module.exports = {
     exportPathMap: function () {
       return {
-        '/': { page: '/dashboard' }, // Set the custom page as the home page
+        '/': { page: '/dashboard' },
       };
     },
   };
